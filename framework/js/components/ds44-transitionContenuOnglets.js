68fdc325563548212557a13efd4502865b5bf03c
DAVID Camille <camille.david@loire-atlantique.fr>
2020-01-10 09:42:53 +0100

    Evol du js des tabulations

---
+++ framework/js/components/ds44-transitionContenuOnglets.js
@@ -0,0 +1,26 @@
+// effectue une transition des display:none sur les contenus des onglets
+function processTransitionOnglets(tabClicked, allTabs) {
+  allTabs.forEach((tab) => {
+
+    let tabpanel = document.querySelector("#" + tab.getAttribute("aria-controls"));
+
+    if (tabClicked == tab) {
+      // On affiche le contenu immédiatement
+      timerClass(tabpanel, "display", "block", 150);
+      timerClass(tabpanel, "opacity", "1", 300);
+
+    } else {
+      // On cache le contenu avec un délai
+      timerClass(tabpanel, "opacity", "0", 150);
+      timerDisplayNone(tabpanel, 150);
+    }
+
+  });
+}
+
+let allTabs = document.querySelectorAll(".js-tablist__link");
+if (allTabs) {
+  allTabs.forEach((tab) => {
+    tab.addEventListener('click', (event) => processTransitionOnglets(event.target, allTabs));
+  });
+}
