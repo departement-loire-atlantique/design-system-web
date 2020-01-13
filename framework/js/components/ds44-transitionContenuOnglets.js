6222adccf2a77ff530a23b009adfa336cb305481
DAVID Camille <camille.david@loire-atlantique.fr>
2020-01-13 10:59:37 +0100

    update fonctionnement onglet accessibilite

--- framework/js/components/ds44-transitionContenuOnglets.js
+++ framework/js/components/ds44-transitionContenuOnglets.js
@@ -1,16 +1,22 @@
 // effectue une transition des display:none sur les contenus des onglets
 function processTransitionOnglets(tabClicked, allTabs) {
+
   allTabs.forEach((tab) => {
 
-    let tabpanel = document.querySelector("#" + tab.getAttribute("aria-controls"));
+    let tabpanel = document.querySelector(tab.getAttribute("href"));
 
     if (tabClicked == tab) {
-      // On affiche le contenu immédiatement
+      tab.setAttribute("aria-selected", "true");
+
       timerClass(tabpanel, "display", "block", 150);
       timerClass(tabpanel, "opacity", "1", 300);
 
+      let tabpanelAnchor = tabpanel.children[0];
+      tabpanelAnchor.focus();
+
     } else {
-      // On cache le contenu avec un délai
+      tab.setAttribute("aria-selected", "false");
+
       tabpanel.style["opacity"] = "0";
       timerDisplayNone(tabpanel, 150);
     }
@@ -21,6 +27,20 @@ function processTransitionOnglets(tabClicked, allTabs) {
 let allTabs = document.querySelectorAll(".js-tablist__link");
 if (allTabs) {
   allTabs.forEach((tab) => {
-    tab.addEventListener('click', (event) => processTransitionOnglets(event.target, allTabs));
+    tab.addEventListener('click', () => processTransitionOnglets(tab, allTabs));
+    tab.addEventListener("keypress", (event) => fusionneKeyPressedWithClicked(event));
+
+    let tabpanel = document.querySelector(tab.getAttribute("href"));
+    let tabpanelExit = tabpanel.children[tabpanel.children.length-1];
+
+    tabpanelExit.addEventListener('click', (event) => { event.preventDefault(); tab.focus(); });
+    tabpanelExit.addEventListener("keypress", (event) => fusionneKeyPressedWithClicked(event));
+
+    if(tab.getAttribute("aria-current") == "true") {
+      tab.focus();
+      tabpanel.style["display"] = "block";
+      timerClass(tabpanel, "opacity", "1", 150);
+    }
+
   });
 }
