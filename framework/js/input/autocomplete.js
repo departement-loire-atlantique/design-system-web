016d8f8638b3c0f9970ffb9d8bdffffe5d1c2865
xavier@altapixel.com <xavier@gibbox.com>
2020-02-07 18:16:00 +0100

    RS-501 - Autocomplete input

--- framework/js/input/autocomplete.js
+++ framework/js/input/autocomplete.js
@@ -19,6 +19,7 @@ class InputAutoComplete extends InputAbstract {
         const object = this.objects[objectIndex];
         object.hiddenInputElement = hiddenInputElement;
         object.autoCompleterElement = null;
+        object.isExpanded = false;
         if (object.containerElement) {
             object.autoCompleterElement = object.containerElement.querySelector('.ds44-autocomp-container');
         }
@@ -28,7 +29,7 @@ class InputAutoComplete extends InputAbstract {
         if (object.autoCompleterListElement) {
             object.autoCompleterListElement.setAttribute('id', 'owned_listbox_' + object.id);
         }
-        if(element.getAttribute('data-mode') === this.SELECT_ONLY_MODE) {
+        if (element.getAttribute('data-mode') === this.SELECT_ONLY_MODE) {
             object.mode = this.SELECT_ONLY_MODE;
         } else {
             object.mode = this.FREE_TEXT_MODE;
@@ -38,8 +39,13 @@ class InputAutoComplete extends InputAbstract {
         this.hide(objectIndex);
 
         MiscEvent.addListener('keyDown:*', this.record.bind(this, objectIndex));
-        MiscEvent.addListener('keyUp:*', this.autoComplete.bind(this, objectIndex));
+        MiscEvent.addListener('keyUp:*', this.write.bind(this, objectIndex));
         MiscEvent.addListener('keyUp:escape', this.hide.bind(this, objectIndex));
+        MiscEvent.addListener('keyUp:spacebar', this.selectOption.bind(this, objectIndex));
+        MiscEvent.addListener('keyUp:enter', this.selectOption.bind(this, objectIndex));
+        MiscEvent.addListener('keyUp:arrowup', this.previousOption.bind(this, objectIndex));
+        MiscEvent.addListener('keyUp:arrowdown', this.nextOption.bind(this, objectIndex));
+
         if (object.containerElement) {
             MiscEvent.addListener('focusout', this.focusOut.bind(this, objectIndex), object.containerElement);
         }
@@ -50,8 +56,23 @@ class InputAutoComplete extends InputAbstract {
         if (!object.inputElement) {
             return;
         }
+        if (object.inputElement !== document.activeElement) {
+            return;
+        }
 
         object.currentInputElementValue = object.inputElement.value;
+    }
+
+    write(objectIndex) {
+        const object = this.objects[objectIndex];
+        if (!object.inputElement) {
+            return;
+        }
+        if (object.inputElement !== document.activeElement) {
+            return;
+        }
+
+        this.autoComplete(objectIndex);
     }
 
     autoComplete(objectIndex) {
@@ -120,9 +141,11 @@ class InputAutoComplete extends InputAbstract {
             elementAutoCompleterListItem.classList.add('ds44-autocomp-list_elem');
             elementAutoCompleterListItem.setAttribute('role', 'option');
             elementAutoCompleterListItem.setAttribute('data-value', key);
+            elementAutoCompleterListItem.setAttribute('tabindex', '0');
             elementAutoCompleterListItem.innerHTML = this.highlightSearch(results.data[key].name, object.inputElement.value);
             object.autoCompleterListElement.appendChild(elementAutoCompleterListItem);
 
+            MiscEvent.addListener('focus', this.fakeSelect.bind(this, objectIndex), elementAutoCompleterListItem);
             MiscEvent.addListener('mousedown', this.select.bind(this, objectIndex), elementAutoCompleterListItem);
         }
 
@@ -136,6 +159,18 @@ class InputAutoComplete extends InputAbstract {
     }
 
     focus(objectIndex) {
+        const object = this.objects[objectIndex];
+        if (!object.inputElement) {
+            return;
+        }
+
+        if (
+            object.currentInputElementValue &&
+            object.currentInputElementValue !== object.inputElement.value
+        ) {
+            object.inputElement.value = object.currentInputElementValue;
+        }
+
         this.autoComplete(objectIndex);
 
         super.focus(objectIndex);
@@ -166,6 +201,7 @@ class InputAutoComplete extends InputAbstract {
             !object.hiddenInputElement.value
         ) {
             object.inputElement.value = null;
+            object.currentInputElementValue = null;
             this.blur(objectIndex);
         }
 
@@ -190,6 +226,7 @@ class InputAutoComplete extends InputAbstract {
         object.autoCompleterElement.classList.remove('hidden');
         MiscAccessibility.show(object.autoCompleterElement, true);
         object.inputElement.setAttribute('aria-expanded', 'true');
+        object.isExpanded = true;
     }
 
     hide(objectIndex) {
@@ -201,9 +238,17 @@ class InputAutoComplete extends InputAbstract {
             return;
         }
 
+        if (
+            object.currentInputElementValue &&
+            object.currentInputElementValue !== object.inputElement.value
+        ) {
+            object.inputElement.value = object.currentInputElementValue;
+        }
+
         object.autoCompleterElement.classList.add('hidden');
         MiscAccessibility.hide(object.autoCompleterElement, true);
         object.inputElement.removeAttribute('aria-expanded');
+        object.isExpanded = false;
     }
 
     highlightSearch(result, search) {
@@ -214,8 +259,84 @@ class InputAutoComplete extends InputAbstract {
         return result.replace(new RegExp(search, 'gi'), str => `<strong>${str}</strong>`);
     }
 
+    fakeSelect(objectIndex, evt) {
+        const object = this.objects[objectIndex];
+        if (!object.inputElement) {
+            return;
+        }
+
+        const currentListItem = evt.currentTarget;
+        object.inputElement.value = currentListItem.innerText;
+        MiscAccessibility.setFocus(currentListItem);
+    }
+
+    selectOption(objectIndex, evt) {
+        evt.preventDefault();
+
+        const object = this.objects[objectIndex];
+        if (!object.autoCompleterListElement) {
+            return;
+        }
+
+        if (
+            document.activeElement &&
+            document.activeElement.classList.contains('ds44-autocomp-list_elem') &&
+            object.autoCompleterListElement.contains(document.activeElement)
+        ) {
+            MiscEvent.dispatch('mousedown', null, document.activeElement);
+        }
+    }
+
+    nextOption(objectIndex, evt) {
+        evt.preventDefault();
+
+        const object = this.objects[objectIndex];
+        if (!object.autoCompleterListElement) {
+            return;
+        }
+
+        if (object.isExpanded) {
+            const selectedListItem = object.autoCompleterListElement.querySelector('.ds44-autocomp-list_elem:focus');
+            const lastListItem = object.autoCompleterListElement.querySelector('.ds44-autocomp-list_elem:last-child');
+            if (
+                !selectedListItem ||
+                selectedListItem === lastListItem
+            ) {
+                // Select first
+                MiscAccessibility.setFocus(object.autoCompleterListElement.querySelector('.ds44-autocomp-list_elem'))
+            } else {
+                // Select next
+                MiscAccessibility.setFocus(MiscDom.getNextSibling(selectedListItem));
+            }
+        }
+    }
+
+    previousOption(objectIndex, evt) {
+        evt.preventDefault();
+
+        const object = this.objects[objectIndex];
+        if (!object.containerElement) {
+            return;
+        }
+
+        if (object.isExpanded) {
+            const selectedListItem = object.autoCompleterListElement.querySelector('.ds44-autocomp-list_elem:focus');
+            const firstListItem = object.autoCompleterListElement.querySelector('.ds44-autocomp-list_elem:first-child');
+            if (
+                !selectedListItem ||
+                selectedListItem === firstListItem
+            ) {
+                // Select last
+                MiscAccessibility.setFocus(object.autoCompleterListElement.querySelector('.ds44-autocomp-list_elem:last-child'))
+            } else {
+                // Select previous
+                MiscAccessibility.setFocus(MiscDom.getPreviousSibling(selectedListItem));
+            }
+        }
+    }
+
     select(objectIndex, evt) {
-        if(evt) {
+        if (evt) {
             evt.preventDefault();
         }
 
