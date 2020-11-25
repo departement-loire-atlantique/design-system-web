af2b9afd4023b9aa264aa9ebca6f64decdc5dcf1
LChoquet <57703110+LChoquet@users.noreply.github.com>
2020-11-25 17:07:05 +0100

    Fix encodage sauts de ligne dans le JS

--- framework/js/misc/accessibility.js
+++ framework/js/misc/accessibility.js
@@ -2,47 +2,47 @@ class MiscAccessibility {
     static getEnabledElementsSelector () {
         return ['a[href]', 'link[href]', 'button', 'textarea', 'input:not([type="hidden"])', 'select', 'object', 'area'].map(selector => selector + ':not([disabled])');
     }
-​
+
     static getProtectedElementsSelector () {
         return ['i', 'sup', 'svg', 'hr'];
     }
-​
+
     // Fonction qui va forcer le focus à faire une boucle sur un élément
     // en ajoutant deux inputs 'hidden' qui peuvent être focus, au début
     // et à la fin
     static addFocusLoop (element) {
         MiscAccessibility.removeFocusLoop();
-​
+
         if (!element) {
             return;
         }
-​
+
         const focusableElements = element.querySelectorAll(MiscAccessibility.getEnabledElementsSelector());
         if (!focusableElements.length) {
             return;
         }
-​
+
         // Add class to first and last focusable elements
         focusableElements[0].classList.add('ds44-tmpFirstFocus');
         focusableElements[focusableElements.length - 1].classList.add('ds44-tmpLastFocus');
-​
+
         // Create first hidden focus element
         const fakeFirstElement = document.createElement('span');
         fakeFirstElement.classList.add('ds44-tmpFocusHidden');
         fakeFirstElement.setAttribute('tabindex', '0');
         element.prepend(fakeFirstElement);
-​
+
         // Create last hidden focus element
         const fakeLastElement = document.createElement('span');
         fakeLastElement.classList.add('ds44-tmpFocusHidden');
         fakeLastElement.setAttribute('tabindex', '0');
         element.appendChild(fakeLastElement);
-​
+
         // Add events
         MiscEvent.addListener('focus', MiscAccessibility.setFocus.bind(this, null, '.ds44-tmpLastFocus'), fakeFirstElement);
         MiscEvent.addListener('focus', MiscAccessibility.setFocus.bind(this, null, '.ds44-tmpFirstFocus'), fakeLastElement);
     }
-​
+
     // Delete loop elements
     static removeFocusLoop () {
         document
@@ -50,18 +50,18 @@ class MiscAccessibility {
             .forEach((element) => {
                 element.remove();
             })
-​
+
         const firstFocusableElement = document.querySelector('.ds44-tmpFirstFocus');
         if (firstFocusableElement) {
             firstFocusableElement.classList.remove('ds44-tmpFirstFocus');
         }
-​
+
         const lastFocusableElement = document.querySelector('.ds44-tmpLastFocus');
         if (lastFocusableElement) {
             lastFocusableElement.classList.remove('ds44-tmpLastFocus');
         }
     }
-​
+
     // Mettre le focus sur un élément précis
     static setFocus (element, selector) {
         if (!element && selector) {
@@ -69,23 +69,23 @@ class MiscAccessibility {
         }
         if (element) {
             let hasTabIndex = element.hasAttribute('tabindex');
-            // Ajouter un tabindex temporaire si l'élément n'en a pas
+            // Ajouter un tabindex temporaire quand il n'y en a pas
             if (!hasTabIndex) {
-            	element.setAttribute('tabindex','-1');
+                element.setAttribute('tabindex','-1');   
             }
             element.focus();
             if (!hasTabIndex) {
-            	element.removeAttribute('tabindex','-1');
+                element.removeAttribute('tabindex');
             }
         }
     }
-​
+
     static show (element, bubble = true, force = true, isChild = false) {
         if (!element) {
             // No element
             return;
         }
-​
+
         if (
             MiscAccessibility.getProtectedElementsSelector().indexOf(element.tagName.toLowerCase()) !== -1 ||
             element.getAttribute('data-a11y-exclude') === 'true'
@@ -93,7 +93,7 @@ class MiscAccessibility {
             // Protected element
             return;
         }
-​
+
         if (!isChild) {
             // Is parent element
             element.removeAttribute('aria-hidden');
@@ -107,7 +107,7 @@ class MiscAccessibility {
                 element.removeAttribute('tabindex');
             }
             element.removeAttribute('data-bkp-tabindex');
-​
+
             if (
                 force &&
                 element.getAttribute('tabindex') === '-1'
@@ -115,20 +115,20 @@ class MiscAccessibility {
                 element.removeAttribute('tabindex');
             }
         }
-​
+
         if (bubble) {
             Array.from(element.children).map((childElement) => {
                 MiscAccessibility.show(childElement, bubble, force, true);
             });
         }
     }
-​
+
     static hide (element, bubble = true, force = true, isChild = false) {
         if (!element) {
             // No element
             return;
         }
-​
+
         if (
             MiscAccessibility.getProtectedElementsSelector().indexOf(element.tagName.toLowerCase()) !== -1 ||
             element.getAttribute('data-a11y-exclude') === 'true'
@@ -136,7 +136,7 @@ class MiscAccessibility {
             // Protected element
             return;
         }
-​
+
         if (!isChild) {
             // Is parent element
             element.setAttribute('aria-hidden', true);
@@ -151,17 +151,17 @@ class MiscAccessibility {
                     element.setAttribute('data-bkp-tabindex', '');
                 }
             }
-​
+
             element.setAttribute('tabindex', '-1');
         }
-​
+
         if (bubble) {
             Array.from(element.children).map((childElement) => {
                 MiscAccessibility.hide(childElement, bubble, force, true);
             });
         }
     }
-​
+
     static flattenText (text) {
         return text.replace(/\n/gi, ' ').replace(/[ ]+/gi, ' ').trim();
     }
