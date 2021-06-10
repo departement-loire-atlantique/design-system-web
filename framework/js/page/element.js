546b7a2239b271db632fe30099da2fc2cc39a4bd
LChoquet <57703110+LChoquet@users.noreply.github.com>
2021-06-10 11:14:50 +0200

    Added scrollToHyperlink function

--- framework/js/page/element.js
+++ framework/js/page/element.js
@@ -12,10 +12,7 @@ class PageElement {
         document
             .querySelectorAll('a[href^="#"]')
             .forEach((link) => {
-                link.addEventListener("click", function(e) {
-                    e.preventDefault();
-                    MiscUtils.scrollTo(document.getElementById(link.getAttribute('href').replace('#', '')));
-                });
+                link.addEventListener("click", this.scrollToHyperlink);
             });
 
         MiscEvent.addListener('overlay:show', this.hide.bind(this));
@@ -50,6 +47,26 @@ class PageElement {
             }
         }
         this.visibilityCounter--;
+    }
+        
+    scrollToHyperlink(event) {
+        event.preventDefault();
+        const scrollTo = MiscUtils.getPositionY(document.getElementById(event.target.getAttribute('href').replace('#', '')));
+        if (MiscUtils.getScrollTop() > scrollTo) {
+            // Going up, the header will show
+            MiscUtils.scrollTo(
+                scrollTo - MiscDom.getHeaderHeight(true),
+                400,
+                'linear'
+            );
+        } else {
+            // Going down, the header will hide
+            MiscUtils.scrollTo(
+                scrollTo,
+                400,
+                'linear'
+            );
+        });
     }
 }
 
