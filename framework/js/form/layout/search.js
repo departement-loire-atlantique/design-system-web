7129a03f36d4d940d9b27415a974105d802b5b5a
Matthieu Beurel <matthieu@mbeurel.com>
2021-12-06 10:25:47 +0100

    fix: RS-2018

--- framework/js/form/layout/search.js
+++ framework/js/form/layout/search.js
@@ -271,7 +271,7 @@ class FormLayoutSearch extends FormLayoutAbstract {
             'nbResults': response['nb-result'],
             'maxNbResults': response['max-result'],
             'results': results,
-            'geojsonId' : response['geojsonId'] ?? {},
+            'geojsonId' : response.hasOwnProperty("geojsonId") ? response['geojsonId'] : null,
             'newResults': response['result'],
             'searchText': searchText.join(', ')
         };
