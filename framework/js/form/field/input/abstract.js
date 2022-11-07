7692d0c2b3740c59b114d73113bf8847673ce40a
Matthieu Beurel <matthieu@mbeurel.com>
2022-11-07 10:24:13 +0100

    AS-124

--- framework/js/form/field/input/abstract.js
+++ framework/js/form/field/input/abstract.js
@@ -15,6 +15,23 @@ class FormFieldInputAbstract extends FormFieldAbstract {
         object.labelElement = MiscDom.getPreviousSibling(element, 'label');
         object.resetButtonElement = MiscDom.getNextSibling(element, '.ds44-reset');
         object.showPasswordButton = MiscDom.getNextSibling(element, ".ds44-showPassword");
+
+        if(object.element.dataset.fieldCompare)
+        {
+            let fieldCompare = document.querySelector(object.element.dataset.fieldCompare);
+            if(fieldCompare) {
+                object.fieldCompare = {
+                    "element":  fieldCompare,
+                    "label":    MiscDom.getPreviousSibling(fieldCompare, 'label'),
+                    "error":    false
+                };
+            }
+        }
+        let fieldsToCompare = document.querySelectorAll("*[data-field-compare='#"+element.getAttribute("id")+"']");
+        if(fieldsToCompare) {
+            object.fieldsToCompare = fieldsToCompare;
+        }
+
     }
 
     initialize () {
@@ -31,6 +48,8 @@ class FormFieldInputAbstract extends FormFieldAbstract {
             MiscEvent.addListener('blur', this.blur.bind(this, objectIndex), object.textElement);
             MiscEvent.addListener('invalid', this.invalid.bind(this, objectIndex), object.textElement);
             MiscEvent.addListener('keyUp:*', this.write.bind(this, objectIndex));
+            MiscEvent.addListener('field:check-value', this.checkValue.bind(this, objectIndex), object.textElement);
+
             if (object.resetButtonElement) {
                 MiscEvent.addListener('click', this.reset.bind(this, objectIndex), object.resetButtonElement);
             }
@@ -58,8 +77,34 @@ class FormFieldInputAbstract extends FormFieldAbstract {
         ) {
             return;
         }
+        this.checkValue(objectIndex);
         this.toggleContainerByValue(objectIndex, object.element.value);
         this.showNotEmpty(objectIndex);
+    }
+
+    checkValue(objectIndex) {
+        const object = this.objects[objectIndex];
+        if (!object) {
+            return;
+        }
+
+        if(object.fieldCompare) {
+            console.log('ici');
+            if(object.fieldCompare.element.value !== null && object.fieldCompare.element.value !== object.element.value) {
+                object.fieldCompare.error = true;
+                this.invalid(objectIndex);
+            }
+            else {
+                object.fieldCompare.error = false;
+                this.removeInvalid(objectIndex);
+            }
+        }
+        if(object.fieldsToCompare.length > 0) {
+            object.fieldsToCompare.forEach((element) => {
+                MiscEvent.dispatch("field:check-value", {}, element);
+            });
+        }
+
     }
 
     showNotEmpty (objectIndex) {
@@ -244,6 +289,13 @@ class FormFieldInputAbstract extends FormFieldAbstract {
             return this.formatErrorMessage(objectIndex);
         }
 
+        if(object.fieldCompare && object.fieldCompare.error) {
+            let pattern = {
+                "fieldNameCompare": object.fieldCompare.label.innerText.replace(/\*$/, '')
+            }
+            return this.formatErrorMessage(objectIndex, 'FIELD_VALID_COMPARE', pattern);
+        }
+
         const autocomplete = object.textElement.getAttribute('autocomplete');
         if (
             autocomplete === 'email' &&
@@ -280,7 +332,6 @@ class FormFieldInputAbstract extends FormFieldAbstract {
         if (!object || !object.textElement) {
             return true;
         }
-
         const data = this.getData(objectIndex);
         if (!data) {
             return true;
