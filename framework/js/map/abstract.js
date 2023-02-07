8fe33bed5d0388a373ca174a6bfc28346b5a8b37
Matthieu Beurel <matthieu@mbeurel.com>
2023-02-07 16:34:11 +0100

    Log Update source

--- framework/js/map/abstract.js
+++ framework/js/map/abstract.js
@@ -164,6 +164,7 @@ class MapAbstract {
             else
             {
                 console.log(object.map.getSource('currentMarker'));
+                console.log(evt);
                 object.map.getSource('currentMarker').setData({
                   'type': 'Feature',
                   'geometry': {
