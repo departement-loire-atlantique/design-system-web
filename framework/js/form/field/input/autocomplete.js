960e93d714ec6043f0f3f9dcb4e578d483d12494
Matthieu Beurel <matthieu@mbeurel.com>
2023-02-07 16:10:32 +0100

    Fix Update source

--- framework/js/form/field/input/autocomplete.js
+++ framework/js/form/field/input/autocomplete.js
@@ -155,7 +155,7 @@ class FormFieldInputAutoCompleteClass extends FormFieldInputAbstract {
 
         const locationElement = object.containerElement.querySelector('.ds44-location');
         if (locationElement) {
-            console.log(locationElement);
+            console.log(object.metadataElement.value);
             document.querySelectorAll(".ds44-js-map").forEach((map) => {
                 map.setAttribute("data-around-me", object.metadataElement.value);
             });
