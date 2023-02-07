12f354028e44e98c46b400d5d4354b2091801478
Matthieu Beurel <matthieu@mbeurel.com>
2023-02-07 14:52:31 +0100

    AS-15

--- framework/js/form/field/input/autocomplete.js
+++ framework/js/form/field/input/autocomplete.js
@@ -155,12 +155,9 @@ class FormFieldInputAutoCompleteClass extends FormFieldInputAbstract {
 
         const locationElement = object.containerElement.querySelector('.ds44-location');
         if (locationElement) {
-            if(object.valueElement.value === "aroundMe")
-            {
-                document.querySelectorAll(".ds44-js-map").forEach((map) => {
-                    map.setAttribute("data-around-me", object.metadataElement.value);
-                });
-            }
+            document.querySelectorAll(".ds44-js-map").forEach((map) => {
+                map.setAttribute("data-around-me", object.metadataElement.value);
+            });
         }
 
     }
