ab59c3cf45584b0d6f572c65e8eed3fe3f0a8bcf
Matthieu Beurel <matthieu@mbeurel.com>
2023-02-07 15:46:55 +0100

    Update location in map

--- framework/js/form/field/input/autocomplete.js
+++ framework/js/form/field/input/autocomplete.js
@@ -157,6 +157,7 @@ class FormFieldInputAutoCompleteClass extends FormFieldInputAbstract {
         if (locationElement) {
             document.querySelectorAll(".ds44-js-map").forEach((map) => {
                 map.setAttribute("data-around-me", object.metadataElement.value);
+                MiscEvent.dispatch("map:aroundMe", {metadata: JSON.parse(object.metadataElement.value)}, object.mapElement);
             });
         }
 
